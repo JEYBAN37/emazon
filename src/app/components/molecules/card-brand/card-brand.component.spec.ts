8518fc551909fd13c7aea7546a3e42fb149813cd
JEYBAN37 <jeyban37@gmail.com>
2024-11-04 10:14:33 -0500

    CREACION DE JU3 Y TEST

--- src/app/components/molecules/card-brand/card-brand.component.spec.ts
+++ src/app/components/molecules/card-brand/card-brand.component.spec.ts
@@ -1,40 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CardCategoryComponent } from './card-category.component';
+import { CardBrandComponent } from './card-brand.component';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { OBJECT_SERVICE, ObjectServiceInterface, ObjectStock } from 'src/app/shared/services/stock-service-interface';
 import { of, throwError } from 'rxjs';
 import { ChangeDetectorRef } from '@angular/core';
 
-describe('CardCategoryComponent', () => {
-  let component: CardCategoryComponent;
-  let fixture: ComponentFixture<CardCategoryComponent>;
+describe('CardBrandComponent', () => {
+  let component: CardBrandComponent;
+  let fixture: ComponentFixture<CardBrandComponent>;
   let mockService: jest.Mocked<ObjectServiceInterface>;
   let changeDetectorRef: ChangeDetectorRef;
 
   beforeEach(async () => {
-    jest.useFakeTimers(); // Usar temporizadores falsos
-
-    // Mock del servicio
+    jest.useFakeTimers(); 
     mockService = {
-      create: jest.fn(),
-      get: jest.fn()
-    } as jest.Mocked<ObjectServiceInterface>;
-
-    await TestBed.configureTestingModule({
-      declarations: [CardCategoryComponent],
-      providers: [
-        FormBuilder,
-        { provide: OBJECT_SERVICE, useValue: mockService },
-        ChangeDetectorRef
-      ],
-      imports: [ReactiveFormsModule]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(CardCategoryComponent);
-    component = fixture.componentInstance;
-    changeDetectorRef = fixture.debugElement.injector.get(ChangeDetectorRef);
-    component.service = mockService;
-    fixture.detectChanges();
+        create: jest.fn(),
+        get: jest.fn()
+      } as jest.Mocked<ObjectServiceInterface>;
+  
+      await TestBed.configureTestingModule({
+        declarations: [CardBrandComponent],
+        providers: [
+          FormBuilder,
+          { provide: OBJECT_SERVICE, useValue: mockService },
+          ChangeDetectorRef
+        ],
+        imports: [ReactiveFormsModule]
+      }).compileComponents();
+  
+      fixture = TestBed.createComponent(CardBrandComponent);
+      component = fixture.componentInstance;
+      changeDetectorRef = fixture.debugElement.injector.get(ChangeDetectorRef);
+      component.service = mockService;
+      fixture.detectChanges();
   });
 
   afterEach(() => {
@@ -48,24 +46,25 @@ describe('CardCategoryComponent', () => {
   describe('initForm', () => {
     it('should initialize the form with default values', () => {
       component.ngOnInit();
-      expect(component.categoryForm.value).toEqual({
+      expect(component.brandForm.value).toEqual({
         name: '',
         description: ''
       });
     });
   });
 
+
   describe('send', () => {
     it('should mark all fields as touched if form is invalid', () => {
-      const markAllAsTouchedSpy = jest.spyOn(component.categoryForm, 'markAllAsTouched');
-      component.categoryForm.controls['name'].setValue(''); // name vacío (inválido)
+      const markAllAsTouchedSpy = jest.spyOn(component.brandForm, 'markAllAsTouched');
+      component.brandForm.controls['name'].setValue(''); // name vacío (inválido)
       component.send();
       expect(markAllAsTouchedSpy).toHaveBeenCalled();
     });
 
     it('should call create on the service if the form is valid', () => {
-      component.categoryForm.controls['name'].setValue('Test Name');
-      component.categoryForm.controls['description'].setValue('Test Description');
+      component.brandForm.controls['name'].setValue('Test Name');
+      component.brandForm.controls['description'].setValue('Test Description');
       
       const objectStock: ObjectStock = { name: 'Test Name', description: 'Test Description' };
       mockService.create.mockReturnValue(of(objectStock));
@@ -76,8 +75,8 @@ describe('CardCategoryComponent', () => {
 
     it('should call handleSuccess on successful submission', () => {
       const handleSuccessSpy = jest.spyOn(component as any, 'handleSuccess');
-      component.categoryForm.controls['name'].setValue('Test Name');
-      component.categoryForm.controls['description'].setValue('Test Description');
+      component.brandForm.controls['name'].setValue('Test Name');
+      component.brandForm.controls['description'].setValue('Test Description');
 
       const response: ObjectStock = { name: 'Test Name', description: 'Test Description' };
       mockService.create.mockReturnValue(of(response));
@@ -88,8 +87,8 @@ describe('CardCategoryComponent', () => {
 
     it('should call handleError on failed submission', () => {
       const handleErrorSpy = jest.spyOn(component as any, 'handleError');
-      component.categoryForm.controls['name'].setValue('Test Name');
-      component.categoryForm.controls['description'].setValue('Test Description');
+      component.brandForm.controls['name'].setValue('Test Name');
+      component.brandForm.controls['description'].setValue('Test Description');
 
       const errorResponse = { error: { message: 'Error' } };
       mockService.create.mockReturnValue(throwError(() => errorResponse));
@@ -127,8 +126,8 @@ describe('CardCategoryComponent', () => {
 
   describe('getFormControl', () => {
     it('should return the FormControl for a given control name', () => {
-      const nameControl = component.getFormControl('name');
-      expect(nameControl).toBe(component.categoryForm.controls['name']);
+      const nameControl = component.nameControl('name');
+      expect(nameControl).toBe(component.brandForm.controls['name']);
     });
     
   });
@@ -136,21 +135,21 @@ describe('CardCategoryComponent', () => {
 
   describe('send', () => {
     it('should mark all fields as touched if form is invalid', () => {
-      const markAllAsTouchedSpy = jest.spyOn(component.categoryForm, 'markAllAsTouched');
-      component.categoryForm.controls['name'].setValue(''); // nombre vacío (inválido)
+      const markAllAsTouchedSpy = jest.spyOn(component.brandForm, 'markAllAsTouched');
+      component.brandForm.controls['name'].setValue(''); // nombre vacío (inválido)
       component.send();
       expect(markAllAsTouchedSpy).toHaveBeenCalled();
     });
 
     it('should not call create on the service if the form is invalid', () => {
-      component.categoryForm.controls['name'].setValue(''); // nombre vacío
+      component.brandForm.controls['name'].setValue(''); // nombre vacío
       component.send();
       expect(mockService.create).not.toHaveBeenCalled(); // Verifica que no se haya llamado al servicio
     });
 
     it('should call create on the service if the form is valid', () => {
-      component.categoryForm.controls['name'].setValue('Test Name');
-      component.categoryForm.controls['description'].setValue('Test Description');
+      component.brandForm.controls['name'].setValue('Test Name');
+      component.brandForm.controls['description'].setValue('Test Description');
       
       const objectStock: ObjectStock = { name: 'Test Name', description: 'Test Description' };
       mockService.create.mockReturnValue(of(objectStock));
@@ -160,8 +159,8 @@ describe('CardCategoryComponent', () => {
     });
     it('should call handleSuccess on successful submission', () => {
       const handleSuccessSpy = jest.spyOn(component as any, 'handleSuccess');
-      component.categoryForm.controls['name'].setValue('Test Name');
-      component.categoryForm.controls['description'].setValue('Test Description');
+      component.brandForm.controls['name'].setValue('Test Name');
+      component.brandForm.controls['description'].setValue('Test Description');
   
       const response: ObjectStock = { name: 'Test Name', description: 'Test Description' };
       mockService.create.mockReturnValue(of(response));
@@ -172,8 +171,8 @@ describe('CardCategoryComponent', () => {
   
   it('should call handleError on failed submission', () => {
       const handleErrorSpy = jest.spyOn(component as any, 'handleError');
-      component.categoryForm.controls['name'].setValue('Test Name');
-      component.categoryForm.controls['description'].setValue('Test Description');
+      component.brandForm.controls['name'].setValue('Test Name');
+      component.brandForm.controls['description'].setValue('Test Description');
   
       const errorResponse = { error: { message: 'Error' } };
       mockService.create.mockReturnValue(throwError(() => errorResponse));
@@ -185,27 +184,26 @@ describe('CardCategoryComponent', () => {
 
   describe('form validations', () => {
     it('should validate the name field as required', () => {
-      const nameControl = component.getFormControl('name');
+      const nameControl = component.nameControl('name');
       nameControl.setValue('');
       expect(nameControl.valid).toBeFalsy();
       expect(nameControl.errors?.['required']).toBeTruthy();
     });
 
     it('should validate the description field as required', () => {
-      const descriptionControl = component.getFormControl('description');
+      const descriptionControl = component.nameControl('description');
       descriptionControl.setValue('');
       expect(descriptionControl.valid).toBeFalsy();
       expect(descriptionControl.errors?.['required']).toBeTruthy();
     });
 
     it('should validate name maximum length', () => {
-      const nameControl = component.getFormControl('name');
+      const nameControl = component.nameControl('name');
       nameControl.setValue('A very long name that exceeds the maximum length allowed');
       expect(nameControl.valid).toBeFalsy();
       expect(nameControl.errors?.['maxlength']).toBeTruthy();
     });
 
   });
-
+ 
 });
-
--- src/app/components/molecules/card-brand/card-brand.component.ts
+++ src/app/components/molecules/card-brand/card-brand.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ObjectServiceInterface } from 'src/app/shared/services/stock-service-interface';
+import { OBJECT_SERVICE, ObjectServiceInterface, ObjectStock } from 'src/app/shared/services/stock-service-interface';
 
 @Component({
   selector: 'app-card-brand',
@@ -14,18 +14,26 @@ export class CardBrandComponent implements OnInit {
   showError: boolean = false;
 
   @Input() service!: ObjectServiceInterface; 
-
+  objectStock!: ObjectStock;
   public brandForm !: FormGroup
 
-  constructor(private fromBuilder : FormBuilder){
-    this.brandForm = this.fromBuilder.group({
-      name:['',[Validators.required, Validators.maxLength(50)]],
-      description:['',[Validators.required, Validators.maxLength(90)]]
-    })
+  constructor(
+    private formBuilder: FormBuilder,
+    private cdr: ChangeDetectorRef,
+    @Inject(OBJECT_SERVICE) private objectService: ObjectServiceInterface 
+  ) {}
+  
+    ngOnInit(): void {
+    this.initForm();
   }
 
-  ngOnInit(): void {
+  private initForm() {
+    this.brandForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(50)]],
+      description: ['', [Validators.required, Validators.maxLength(90)]]
+    });
   }
+
 
   send() {
     if (this.brandForm.invalid) {
@@ -34,40 +42,44 @@ export class CardBrandComponent implements OnInit {
     }
 
     const objectStock = this.brandForm.value;
-    console.log("Formulario enviado");
+
 
     this.service.create(objectStock).subscribe({
-      next: (response) => {
-        console.log('Respuesta del backend:', response);
-        this.successMessage = "Formulario enviado exitosamente";
-        this.showSuccess = true; // Muestra el mensaje de éxito
-        this.errorMessage = null;
-
-        // Desaparece el mensaje después de 5 segundos
-        setTimeout(() => {
-          this.showSuccess = false;
-        }, 5000);
-      },
-      error: (error) => {
-        console.error('Error al enviar solicitud:', error);
-        
-        if (error.error && error.error.message) {
-          this.errorMessage = error.error.message;
-        } else {
-          this.errorMessage = "Hubo un error al enviar el formulario";
-        }
-        this.showError = true; // Muestra el mensaje de error
-
-        // Desaparece el mensaje después de 5 segundos
-        setTimeout(() => {
-          this.showError = false;
-        }, 5000);
-        
-        this.successMessage = null;
-      }
+      next: (response) => this.handleSuccess(response),
+      error: (error) => this.handleError(error)
     });
   }
-  
+
+  private handleSuccess(response: any) {
+    console.log('Respuesta del backend:', response);
+    this.successMessage = "Formulario enviado exitosamente";
+    this.showSuccess = true;
+    this.errorMessage = null;
+
+    this.cdr.detectChanges(); // Actualiza la vista
+
+    setTimeout(() => {
+      this.showSuccess = false;
+      this.cdr.detectChanges();
+    }, 2000);
+  }
+
+  private handleError(error: any) {
+    console.error('Error al enviar solicitud:', error);
+    
+    this.errorMessage = error.error?.message || "Hubo un error al enviar";
+    this.showError = true;
+    this.successMessage = null;
+
+    this.cdr.detectChanges();
+
+    setTimeout(() => {
+      this.showError = false;
+      this.cdr.detectChanges();
+    }, 2000);
+  }
+
+
   nameControl(variable : string): FormControl {
     return this.brandForm.get(variable) as FormControl;
   }
